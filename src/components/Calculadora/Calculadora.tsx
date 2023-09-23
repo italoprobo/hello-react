91886c21a607a79395d88463045b72c57f529913
italoprobo <italoprobo@gmail.com>
2023-09-22 21:04:05 -0300

    update react milhas

--- src/components/Calculadora/Calculadora.tsx
+++ src/components/Calculadora/Calculadora.tsx
@@ -21,67 +21,69 @@ const MilesInput: React.FC<{
   onDiscountChange,
   onBonusPercentageChange,
 }) => {
-  const decreaseMiles = (amount: number) => {
-    onMilesChange(Math.max(0, miles - amount));
-  };
+    const [bonusInputValue, setBonusInputValue] = useState(bonusPercentage);
 
-  const increaseMiles = (amount: number) => {
-    onMilesChange(miles + amount);
-  };
 
-  const applyBonus = (percentage: number) => {
-    onBonusPercentageChange(percentage);
-    const bonusMiles = (miles / 100) * percentage;
-    onMilesChange(miles + bonusMiles);
-  };
+    const decreaseMiles = (amount: number) => {
+      onMilesChange(Math.max(0, miles - amount));
+    };
 
-  return (
-    <div>
-      <h5>Milhas</h5>
-      <div className="miles-input">
-        <button onClick={() => decreaseMiles(10000)}>+10k</button>
-        <button onClick={() => decreaseMiles(1000)}>+1k</button>
+    const increaseMiles = (amount: number) => {
+      onMilesChange(miles + amount);
+    };
+
+    const applyBonus = () => {
+      onBonusPercentageChange(bonusInputValue);
+    };
+
+    return (
+      <div>
+        <h5>Milhas</h5>
+        <div className="miles-input">
+          <button onClick={() => decreaseMiles(10000)}>-10k</button>
+          <button onClick={() => decreaseMiles(1000)}>-1k</button>
+          <input
+            type="number"
+            value={miles}
+            onChange={(e) => onMilesChange(Number(e.target.value))}
+          />
+          <button onClick={() => increaseMiles(1000)}>+1k</button>
+          <button onClick={() => increaseMiles(10000)}>+10k</button>
+        </div>
+
+        <h5>Preço milheiro</h5>
         <input
           type="number"
-          value={miles}
-          onChange={(e) => onMilesChange(Number(e.target.value))}
+          value={pricePerMilheiro}
+          onChange={(e) => onPricePerMilheiroChange(Number(e.target.value))}
         />
-        <button onClick={() => increaseMiles(1000)}>-1k</button>
-        <button onClick={() => increaseMiles(10000)}>-10k</button>
-      </div>
 
-      <h5>Preço milheiro</h5>
-      <input
-        type="number"
-        value={pricePerMilheiro}
-        onChange={(e) => onPricePerMilheiroChange(Number(e.target.value))}
-      />
-
-      <h5>Desconto (%)</h5>
-      <input
-        type="number"
-        value={discount}
-        onChange={(e) => onDiscountChange(Number(e.target.value))}
-      />
-
-      <h5>Bônus (%)</h5>
-      <div className="bonus-input">
+        <h5>Desconto (%)</h5>
         <input
           type="number"
-          value={bonusPercentage}
-          onChange={(e) => onBonusPercentageChange(Number(e.target.value))}
+          value={discount}
+          onChange={(e) => onDiscountChange(Number(e.target.value))}
         />
-        <div>
-          <button onClick={() => applyBonus(80)}>80%</button>
-          <button onClick={() => applyBonus(100)}>100%</button>
-          <button onClick={() => applyBonus(150)}>150%</button>
-          <button onClick={() => applyBonus(200)}>200%</button>
-          <button onClick={() => applyBonus(300)}>300%</button>
+
+        <h5>Bônus (%)</h5>
+        <div className="bonus-input">
+          <input
+            type="number"
+            value={bonusInputValue}
+            onChange={(e) => setBonusInputValue(Number(e.target.value))}
+          />
+          <div>
+            <button onClick={() => setBonusInputValue(80)}>80%</button>
+            <button onClick={() => setBonusInputValue(100)}>100%</button>
+            <button onClick={() => setBonusInputValue(150)}>150%</button>
+            <button onClick={() => setBonusInputValue(200)}>200%</button>
+            <button onClick={() => setBonusInputValue(300)}>300%</button>
+            <button onClick={applyBonus}>Calcular</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 const Calculadora: React.FC = () => {
   const [miles, setMiles] = useState(0);
@@ -100,7 +102,6 @@ const Calculadora: React.FC = () => {
 
   return (
     <div className="calculator">
-      <h2>Simulador de Milhas</h2>
 
       <MilesInput
         miles={miles}
@@ -112,6 +113,10 @@ const Calculadora: React.FC = () => {
         onDiscountChange={setDiscount}
         onBonusPercentageChange={setBonusPercentage}
       />
+
+      <div>
+        <strong>Milhas:</strong> {miles} milhas
+      </div>
 
       <div>
         <strong>Milhas com Bônus:</strong> {calculateTotalMilesWithBonus()} milhas
